7670939a397ed7f4775c1ebad3989e56ed31d79a
Rose Porta <rporta@smith.edu>
2022-04-22 11:46:14 -0400

    add error messages and update documentation

--- R/sample_people.R
+++ R/sample_people.R
@@ -32,18 +32,33 @@ sample_people_many <- function(data, num_people = 1000) {
 }
 
 #' Generate dataframe with locations of dots representing people on map
-#' @param data A dataframe containing shapefiles to plot census tract boundaries for each year, output of get_data_wide function
+#' @param data A dataframe containing shapefiles to plot census tract boundaries for each year, output of filter_data_wide function
 #' @param num_people number of individuals that each dot represents
 #' @export
+#' @examples
+#' \dontrun{
+#' create_dots(filter_data_wide(get_data_wide(), "New York", "Madison"))
+#' }
 create_dots <- function(data, num_people = 100) {
+  if(!is.data.frame(data)) stop("invalid input, 'data' argument must have class data.frame")
+  if(!is.numeric(num_people)) {
+    message("'num_people' argument must have class numeric, setting num_people to default value of 100")
+    num_people <- 100
+  }
+  message("This may take a few minutes to several hours, depending on the size of the county and the value of the 'num_people' argument...")
+  num_people <- round(num_people)
+  
   # resolve non-spherical coordinates error
   sf::sf_use_s2(FALSE)
-  data %>% 
-    dplyr::mutate(
-      people = purrr::map(tract_data, sample_people_many, num_people = num_people)
-    ) %>% 
-    dplyr::select(year, people) %>% 
-    tidyr::unnest(cols = people) %>% 
-    dplyr::left_join(censusviz::census_var_map, by = c("year", "variable")) %>% 
-    sf::st_as_sf()
+  
+  suppressMessages(
+    data %>% 
+      dplyr::mutate(
+        people = purrr::map(tract_data, sample_people_many, num_people = num_people)
+      ) %>% 
+      dplyr::select(year, people) %>% 
+      tidyr::unnest(cols = people) %>% 
+      dplyr::left_join(censusviz::census_var_map, by = c("year", "variable")) %>% 
+      sf::st_as_sf()
+  )
 }
