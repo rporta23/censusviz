e963a71e51c27fc11afff48127fbd4a218f7015c
Irene Foster <ifoster25@smith.edu>
2022-04-26 11:34:34 -0400

    changing variable names to camel case

--- data-raw/loadData.R
+++ data-raw/loadData.R
@@ -12,30 +12,30 @@ boston_sample <- data_wide %>%
 
 
 #' Function to pre-load the population of Manhattan, New York
-manhattanSample <- data_wide %>%
+manhattan_sample <- data_wide %>%
   filter_data_wide("New York", "Manhattan") %>%
   create_dots()
 
 
 #' Function to pre-load the population of Chicago Illinois
-chicagoSample <- data_wide %>%
+chicago_sample <- data_wide %>%
   filter_data_wide("Illinois", "Cook") %>%
   create_dots()
 
 
 #' Function to pre-load the population of Austin, Texas
-austinSample <- data_wide %>%
+austin_sample <- data_wide %>%
   filter_data_wide("Texas", "Austin") %>%
   create_dots()
 
 
 #' Function to pre-load the population of San Francisco, California
-sanfranciscoSample <- data_wide %>%
+sanfrancisco_sample <- data_wide %>%
   filter_data_wide("California", "San Francisco") %>%
   create_dots()
 
 
 #' Function to pre-load the population of Seattle, Washington
-seattleSample <- data_wide %>%
+seattle_sample <- data_wide %>%
   filter_data_wide("Washington", "King") %>%
   create_dots()
